b1849f56fa9d3b7a5d78b0a6951a445fe0930580
lcappellini <168109119+lcappellini@users.noreply.github.com>
2024-06-21 02:24:34 +0200

    Made some changes

--- db_utils.py
+++ db_utils.py
@@ -15,9 +15,9 @@ conditions = [
     "Windy"
 ]
 
-
+# erases all the db and repopulates the tables with some example data
 def reset_db(app, db):
-    from main import Place, Condition, Forecast, CurrentWeather, User
+    from main import Place, Condition, Forecast, User
 
     with app.app_context():
         db.create_all()
@@ -36,15 +36,19 @@ def reset_db(app, db):
         #
         db.session.add(Place(id=1, name="Firenze", center_lat=43.77, center_lon=11.2577, radius=4.5))
         db.session.add(Place(id=2, name="Prato", center_lat=43.8777, center_lon=11.1022, radius=0))
+        db.session.add(Place(id=3, name="Roma", center_lat=43.8777, center_lon=11.1022, radius=0))
 
         # add sample entities to db for testing purpose
         db.session.add(Forecast(placeid=1, date=datetime(2024, 6, 19), condition=1, temperature=31.3, rain=0, humidity=30, wind=3, wind_direction="N"))
-        db.session.add(Forecast(placeid=2, date=datetime(2024, 6, 19), condition=1, temperature=35.1, rain=0, humidity=32, wind=5, wind_direction="NE"))
+        db.session.add(Forecast(placeid=2, date=datetime(2024, 6, 19), condition=1, temperature=30.1, rain=0, humidity=32, wind=5, wind_direction="NE"))
+        db.session.add(Forecast(placeid=3, date=datetime(2024, 6, 19), condition=2, temperature=32.1, rain=0, humidity=32, wind=5, wind_direction="NO"))
 
         db.session.add(Forecast(placeid=1, date=datetime(2024, 6, 20), condition=2, temperature=29.4, rain=0, humidity=35, wind=8, wind_direction="S"))
         db.session.add(Forecast(placeid=2, date=datetime(2024, 6, 20), condition=2, temperature=27.5, rain=0, humidity=40, wind=7, wind_direction="SO"))
+        db.session.add(Forecast(placeid=3, date=datetime(2024, 6, 20), condition=3, temperature=24.1, rain=0, humidity=42, wind=7, wind_direction="SE"))
 
         db.session.add(Forecast(placeid=1, date=datetime(2024, 6, 21), condition=3, temperature=28.7, rain=0, humidity=30, wind=4, wind_direction="E"))
         db.session.add(Forecast(placeid=2, date=datetime(2024, 6, 21), condition=3, temperature=27.3, rain=0, humidity=30, wind=9, wind_direction="E"))
+        db.session.add(Forecast(placeid=3, date=datetime(2024, 6, 21), condition=5, temperature=19.8, rain=1, humidity=52, wind=8, wind_direction="O"))
 
         db.session.commit()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from math import radians, asin, sqrt, sin, cos
 
 from flask import Flask, request, render_template, session, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -15,9 +16,8 @@ db = SQLAlchemy(app)
 class Place(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
-    center_lat = db.Column(db.Float)
-    center_lon = db.Column(db.Float)
-    radius = db.Column(db.Float)
+    lat = db.Column(db.Float)
+    lon = db.Column(db.Float)
 
 
 class Condition(db.Model):
@@ -38,17 +38,6 @@ class Forecast(db.Model):
     wind_direction = db.Column(db.String(5))
 
 
-class CurrentWeather(db.Model):
-    placeid = db.Column(db.Integer, primary_key=True)
-
-    condition = db.Column(db.Integer)
-    temperature = db.Column(db.Integer)
-    rain = db.Column(db.Float)
-    humidity = db.Column(db.Integer)
-    wind = db.Column(db.Integer)
-    wind_direction = db.Column(db.String(5))
-
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
@@ -57,6 +46,7 @@ class User(db.Model):
 
 
 from db_utils import reset_db
+
 
 import hashlib
 def hash_password(password):
@@ -77,11 +67,8 @@ def validate_key(key):
 def to_dict(obj):
     return {c.name: getattr(obj, c.name) for c in obj.__table__.columns}
 
-def get_placeid(coords=None, placename=None):
-    if coords is not None:
-        # TODO check if coords inside the area created with center and radius
-        return Place.query.filter_by(center_lat=coords[0], center_lon=coords[1]).first().id
-    elif placename is not None:
+def get_placeid(placename=None):
+    if placename is not None:
         place = Place.query.filter(func.lower(Place.name) == func.lower(placename)).first()
         if place is None:
             return None
@@ -114,6 +101,14 @@ def login():
         else:
             session['username'] = username
             return redirect('/dashboard')
+
+
+@app.route("/logout")
+def logout():
+    if request.method == "GET":
+        if "username" in session:
+            del session["username"]
+    return redirect('/')
 
 
 @app.route("/register", methods=['POST', 'GET'])
@@ -179,21 +174,15 @@ def forecastAPI():
 
         if "placename" in request.args:
             placename = request.args["placename"]
-            placeid = get_placeid(placename=placename)
-        elif "lat" in request.args and "lon" in request.args:
-            lat = request.args["lat"]
-            lon = request.args["lon"]
-            placeid = get_placeid(coords=(lat, lon))
-        elif "coords" in request.args:
-            coords = request.args["coords"]
-            placeid = get_placeid(coords=coords)
+            place = Place.query.filter(func.lower(Place.name) == func.lower(placename)).first()
+            if place is None:
+                return create_error_json(400, "Place not found. Check the value or use another parameter to specify a place.")
+            else:
+                placeid = place.id
         elif "placeid" in request.args:
             placeid = request.args["placeid"]
         else:
-            return create_error_json(400, "Missing required parameter, one of the following must be used: 'placename', 'lat, lon', 'coords' or 'placeid'.")
-
-        if placeid is None:
-            return create_error_json(400, "Place not found. Check the value or use another parameter to specify a place.")
+            return create_error_json(400, "Missing required parameter, one of the following must be used: 'placename' or 'placeid'.")
 
         if "date" in request.args:
             try:
@@ -274,7 +263,7 @@ def forecastAPI():
     elif request.method == "DELETE":
         if "key" in request.form:
             if not validate_key(request.form["key"]):
-                return create_error_json(401, "Unauthorized, invalid or missing API key.")
+                return create_error_json(401, "Unauthorized or invalid API key.")
         else:
             return create_error_json(400, "Missing required parameter 'key'. Request an API key from the homepage.")
 
@@ -292,82 +281,54 @@ def forecastAPI():
         return "Deleted", 200
 
 
-@app.route("/api/now", methods=["GET", "PUT"])
-def nowAPI():
-    if request.method == "GET":
-        if "placename" in request.args:
-            placename = request.args["placename"]
-            placeid = get_placeid(placename=placename)
-        elif "lat" in request.args and "lon" in request.args:
-            lat = request.args["lat"]
-            lon = request.args["lon"]
-            placeid = get_placeid(coords=(lat, lon))
-        elif "coords" in request.args:
-            coords = request.args["coords"]
-            placeid = get_placeid(coords=coords)
-        elif "placeid" in request.args:
-            placeid = request.args["placeid"]
-        else:
-            return create_error_json(400, "Missing required parameter, one of the following must be used: 'placename', 'lat, lon', 'coords' or 'placeid'.")
-
-        if placeid is None:
-            return create_error_json(400, "Place not found. Check the value or use another parameter to specify a place.")
-
-        if "date" in request.args:
-            try:
-                date = datetime.fromisoformat(request.args["date"])
-            except ValueError:
-                return create_error_json(400, "Unable to parse 'date', provide it with the format 'yyyy-mm-dd' or 'yyyymmdd'")
-        else:
-            date = datetime.today()
-
-        details = False
-        if "details" in request.args:
-            if request.args["details"].lower() == "true":
-                details = True
-            elif request.args["details"].lower() == "false":
-                pass
-            else:
-                return create_error_json(400, "Unable to parse 'details', value must be 'true' or 'false'.")
-
-        current_weather = CurrentWeather.query.filter_by(placeid=placeid, date=date).first()
-
-        if current_weather is None:
-            return create_error_json(404, "Nothing was found in the database with the parameters specified.")
-        else:
-            result = {"id": current_weather.id,
-                      "placeid": current_weather.placeid,
-                      "date": current_weather.date,
-                      "condition": current_weather.condition,
-                      "temperature": current_weather.temperature,
-                      "rain": current_weather.rain,
-                      "humidity": current_weather.humidity,
-                      "wind": current_weather.wind,
-                      "wind_direction": current_weather.wind_direction}
-            if details:
-                condition = Condition.query.filter_by(id=current_weather.condition).first()
-                result["condition_description"] = condition.description
-                place = Place.query.filter_by(id=current_weather.placeid).first()
-                result["placename"] = place.name
-            return result
-
-
-@app.route("/api/places", methods=["GET"])
+@app.route("/api/places", methods=["GET", "PUT"])
 def placesAPI():
     if request.method == "GET":
         if "placename" in request.args:
             place = Place.query.filter(func.lower(Place.name) == func.lower(request.args["placename"])).first()
-        elif "lat" in request.args and "lon" in request.args:
-            place = Place.query.filter_by(coords=(request.args["lat"], request.args["lon"])).first()
-        elif "coords" in request.args:
-            place = Place.query.filter_by(coords=request.args["coords"]).first()
+        elif "placeid" in request.args:
+            place = Place.query.filter(func.lower(Place.name) == func.lower(request.args["placename"])).first()
         else:
-            return create_error_json(400, "Missing required parameter, one of the following must be used: 'placename', 'lat, lon' or 'coords'")
+            return create_error_json(400, "Missing required parameter, one of the following must be used: 'placename', 'placeid'")
 
         if place is None:
             return create_error_json(404, "Nothing was found in the database with the parameters specified.")
         else:
             return to_dict(place)
+
+    elif request.method == "PUT":
+        if "key" in request.form:
+            if not validate_key(request.form["key"]):
+                return create_error_json(401, "Unauthorized or invalid API key.")
+        else:
+            return create_error_json(400, "Missing required parameter: key. Request an API key from the homepage.")
+
+        if "name" in request.form:
+            placename = request.form["placename"]
+        else:
+            return create_error_json(400, "Missing required parameter 'name'")
+
+        coords = request.form.get("coords", None)
+        lat = request.form.get("lat", None)
+        lon = request.form.get("lon", None)
+        if lat and lon:
+            pass
+        elif coords:
+            try:
+                lat, lon = coords.split(",")
+                lat = float(lat)
+                lon = float(lon)
+            except:
+                return create_error_json(400, "Unable to parse 'coords', value must be of format 'float,float'.")
+        else:
+            return create_error_json(400, "Missing required parameter, one of the following must be used: 'coords' or 'lat' and 'lon'.")
+
+        place = Place(name=placename,
+                      lat=lat,
+                      lon=lon)
+
+        db.session.add(place)
+        db.session.commit()
 
 
 @app.route("/api/conditions", methods=["GET"])
@@ -410,4 +371,4 @@ def alltables():
 
 if __name__ == "__main__":
     # reset_db(app, db)
-    app.run()
+    app.run(debug=False)
